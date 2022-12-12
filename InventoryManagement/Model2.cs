00b3ae6138fac73298b5dfd3379bce7db023a581
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-12 21:28:56 +0530

    [Rutuja]=Json Inventory Management

---
+++ InventoryManagement/Model2.cs
@@ -0,0 +1,17 @@
+﻿using JsonInventory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class Model2
+    {
+        public List<Model> Rice { get; set; }
+        public List<Model> Wheat { get; set; }
+        public List<Model> Pulses { get; set; }
+
+    }
+}
